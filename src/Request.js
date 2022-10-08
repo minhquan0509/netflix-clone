55607e2d3b984e099675240b5cb819d101a6efac
MinhQuan <minkkquan2@gmail.com>
2022-10-08 10:52:03 +0700

    base project

---
+++ src/Request.js
@@ -0,0 +1,11 @@
+const API_KEY = 'f4afcb73934153e6a4b3b8dbb0d53d6e';
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+};
+
+export default requests;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Banner from '../components/banner/Banner';
+import './HomeScreen.css';
+import Nav from '../components/nav/Nav';
+import requests from '../Request';
+import Row from '../components/row/Row';
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            <Nav />
+            <Banner />
+            <Row title="TRENDING" fetchUrl={requests.fetchTrending} isLargeRow />
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+            <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+        </div>
+    );
+}
+
+export default HomeScreen;
